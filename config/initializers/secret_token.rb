ea33f1267a71e4029f58d8cb63bc66b9a6a56e91
Kohei Iwasaki <jupiter@sophia.ac.jp>
2013-03-20 18:59:54 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Splog::Application.config.secret_key_base = '53516b06f317b083da75950edac4178ae853257121caa3625802f93d3f324b71c35c2c1043085e787ba156f01497a5b0424bfabe91364f3aee4ecb9a88e45ccb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Splog::Application.config.session_store :encrypted_cookie_store, key: '_splog_session'
